b27c3fcd71d0baab7d9260d878c0648bf76a0460
DIEGO RODRIGO BARBOSA VITAL <diego_vital@msn.com>
2026-08-24 16:45:07 -0300

    adds Linting Github Action

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,49 +1,48 @@
 import { runner } from "node-pg-migrate";
 import { join } from "node:path";
-import  database  from "infra/database.js";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
-  if (!allowedMethods.includes(request.method)){
+  if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
       error: `Method "${request.method}" not allowed`,
     });
   }
 
   let dbClient;
-  
-  try{
+
+  try {
     dbClient = await database.getNewClient();
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: join("infra","migrations"),
+      dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
-      migrationsTable: "pgmigrations"
-    }
+      migrationsTable: "pgmigrations",
+    };
 
-    if(request.method === "GET"){
+    if (request.method === "GET") {
       const pendingMigrations = await runner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
     }
 
-    if(request.method === "POST"){
+    if (request.method === "POST") {
       const migratedMigrations = await runner({
         ...defaultMigrationOptions,
         dryRun: false,
       });
 
-      if(migratedMigrations.length > 0){
+      if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
       return response.status(200).json(migratedMigrations);
     }
-
-  }catch (error){
+  } catch (error) {
     console.error(error);
     throw error;
-  }finally {
+  } finally {
     await dbClient.end();
   }
 }
