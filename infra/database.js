08f41a4ba16aff2491f23b828adc33771b9aa204
DIEGO RODRIGO BARBOSA VITAL <diego_vital@msn.com>
2026-09-23 17:37:35 -0300

    refactor: use next-connect in /migrations and /status

--- infra/database.js
+++ infra/database.js
@@ -8,11 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    const ServiceErrorObject = new ServiceError({
+    const serviceErrorObject = new ServiceError({
       message: "Erro na conexão com Banco ou na Query.",
       cause: error,
     });
-    throw error;
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
