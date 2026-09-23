01e0e7886d0d4d803b70b89d5b79868863cec4f5
DIEGO RODRIGO BARBOSA VITAL <diego_vital@msn.com>
2026-09-23 17:37:01 -0300

    feat: use ServiceError in infra/database.js

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,8 +8,10 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\nErro dentro do catch do controller:");
-    console.log(error);
+    const ServiceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
     throw error;
   } finally {
     await client?.end();
